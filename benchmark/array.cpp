b680a597b93d106f30fe9494e124d174b2444443
Alan Park <alanpark.jaewoo@gmail.com>
2019-04-19 09:47:38 +0900

    Refactor array benchmark code to test the case of a large array

--- benchmark/array.cpp
+++ benchmark/array.cpp
@@ -4,22 +4,24 @@
 #include <benchmark/benchmark.h>
 #include "../src/array.h"
 
+const auto size = (1 << 18) + 1;
+
 static void BM_Array_Set_Test(benchmark::State& state) {
-    auto array = Array<int, 100>();
+    auto array = Array<int, size>();
     for(auto _ : state) {
-        array.set(state.range(0) % 100, 1);
+        array.set(state.range(0), 1);
     }
-    state.SetComplexityN(state.range(0) % 100);
+    state.SetComplexityN(state.range(0));
 }
 
 BENCHMARK(BM_Array_Set_Test) -> RangeMultiplier(2) -> Range(1<<10, 1<<18) -> Complexity();
 
 static void BM_Array_Get_Test(benchmark::State& state) {
-    auto array = Array<int, 100>();
+    auto array = Array<int, size>();
     for(auto _ : state) {
-        array.get(state.range(0) % 100);
+        array.get(state.range(0));
     }
-    state.SetComplexityN(state.range(0) % 100);
+    state.SetComplexityN(state.range(0));
 }
 
 BENCHMARK(BM_Array_Get_Test) -> RangeMultiplier(2) -> Range(1<<10, 1<<18) -> Complexity();
